11ab21f80dce921118db822485e9330ac1d1756f
Dang Nguyen Minh <Dang@Dangs-MacBook-Air.local>
2015-09-24 16:50:07 +0700

    init

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+FirebaseTest::Application.config.secret_key_base = 'f554a99dee34e761daa7e579f98196e321dfb2708554d68e548cae339f9e2a640de017961f07770be6fb0192e7058b2648e952d41a04a2cc2c15b0f58a915b16'
